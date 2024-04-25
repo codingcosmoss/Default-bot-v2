85285e275aa0671eb719377dd44bf5c29bb1c6b1
Muhammadali <muhammadaliwebdeveloper@gmail.com>
2024-04-25 14:31:51 +0500

    bug fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,16 +16,16 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-         \App\Models\User::factory()->create([
-             'name' => 'Super admin',
-             'login' => 'admin',
-             'password' =>  Hash::make('560605860608') , // avvalgisi 121212
-             'position' => 'Admin',
-             'role' => 'admin'
-         ]);
+//         \App\Models\User::factory()->create([
+//             'name' => 'Super admin',
+//             'login' => 'admin',
+//             'password' =>  Hash::make('560605860608') , // avvalgisi 121212
+//             'position' => 'Admin',
+//             'role' => 'admin'
+//         ]);
         $this->call([
-            PaymentTypeSeeder::class,
-            ServiceCategorySeeder::class,
+//            PaymentTypeSeeder::class,
+//            ServiceCategorySeeder::class,
             RoleSeeder::class,
             PermissionSeeder::class,
             AdminAddPermissionSeeder::class
